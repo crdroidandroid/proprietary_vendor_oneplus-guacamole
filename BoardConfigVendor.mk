abd37015bcfbf371c9892a6ec3187b2de6977e91
ShevT <evgeny.shishkov@gmail.com>
2025-07-14 14:55:31 +0300

    guacamole: Drop firmware extraction
    Change-Id: I8fdc4e5d86531ef66becaf990152332fd3c31ce6

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,21 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-AB_OTA_PARTITIONS += \
-    LOGO \
-    abl \
-    aop \
-    bluetooth \
-    cmnlib \
-    cmnlib64 \
-    devcfg \
-    dsp \
-    hyp \
-    keymaster \
-    modem \
-    qupfw \
-    storsec \
-    tz \
-    xbl \
-    xbl_config
